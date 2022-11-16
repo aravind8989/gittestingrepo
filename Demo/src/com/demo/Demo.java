a45e3f34dc683bce106a74b3939710eae298fe01
Admin <TL>
2022-11-16 20:18:24 +0530

    Developer Modified

--- Demo/src/com/demo/Demo.java
+++ Demo/src/com/demo/Demo.java
@@ -1,6 +1,11 @@
 package com.demo;
+
+import com.demo.test.Utility;
+
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		Utility utility=new Utility();
+		System.out.println(utility.getSysDate());		
 	}
 }
---
+++ Demo/src/com/demo/test/Utility.java
@@ -0,0 +1,9 @@
+package com.demo.test;
+
+import java.util.Date;
+
+public class Utility {
+	public Date getSysDate() {
+		return new Date();
+	}
+}
